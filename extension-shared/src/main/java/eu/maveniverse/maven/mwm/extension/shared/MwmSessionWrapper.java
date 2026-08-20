756ee223bf0ccbe67a4366a250e868321a7051fc
Tamas Cservenak <tamas@cservenak.net>
2026-08-20 20:49:51 +0200

    Share code, keep actual extensions thin

---
+++ extension-shared/src/main/java/eu/maveniverse/maven/mwm/extension/shared/MwmSessionWrapper.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2023-2024 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+package eu.maveniverse.maven.mwm.extension.shared;
+
+import static java.util.Objects.requireNonNull;
+
+import eu.maveniverse.maven.mwm.core.Workspace;
+import eu.maveniverse.maven.mwm.core.WorkspaceManager;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.repository.LocalRepository;
+import org.eclipse.aether.repository.LocalRepositoryManager;
+import org.eclipse.aether.util.repository.ChainedLocalRepositoryManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+@Named
+public final class MwmSessionWrapper {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final RepositorySystem repositorySystem;
+    private final WorkspaceManager workspaceManager;
+
+    @Inject
+    public MwmSessionWrapper(RepositorySystem repositorySystem, WorkspaceManager workspaceManager) {
+        this.repositorySystem = requireNonNull(repositorySystem);
+        this.workspaceManager = requireNonNull(workspaceManager);
+    }
+
+    public RepositorySystemSession.SessionBuilder wrap(
+            Path projectRoot, RepositorySystemSession.SessionBuilder builder) {
+        return builder.setLocalRepositoryManager(newLocalRepositoryManager(projectRoot, builder));
+    }
+
+    private LocalRepositoryManager newLocalRepositoryManager(
+            Path projectRoot, RepositorySystemSession.SessionBuilder builder) {
+        try (RepositorySystemSession.CloseableSession session = builder.build()) {
+            if (session.getLocalRepositoryManager() instanceof ChainedLocalRepositoryManager) {
+                logger.info("Chained LRM detected; MWM is not interfering with it");
+                return session.getLocalRepositoryManager();
+            }
+
+            Map<String, String> configProperties = new HashMap<>();
+            session.getConfigProperties().forEach((key, value) -> {
+                if (value instanceof String) {
+                    configProperties.put(key, (String) value);
+                }
+            });
+            Workspace workspace = workspaceManager
+                    .detectWorkspace(projectRoot, configProperties)
+                    .orElse(null);
+            if (workspace != null) {
+                logger.info("Using MWM workspace: {}", workspace.workspaceId());
+                Path buildOutputDirectory = session.getLocalRepository()
+                        .getBasePath()
+                        .resolve(".mwn")
+                        .resolve(workspace.workspaceId());
+                return new ChainedLocalRepositoryManager(
+                        session.getLocalRepositoryManager(),
+                        Collections.singletonList(repositorySystem.newLocalRepositoryManager(
+                                session, new LocalRepository(buildOutputDirectory))),
+                        false,
+                        1,
+                        0);
+            }
+
+            return session.getLocalRepositoryManager();
+        }
+    }
+}
--- extension3.10/src/main/java/eu/maveniverse/maven/mwm/extension310/internal/MwmRepositorySystemSessionFactory.java
+++ extension3.10/src/main/java/eu/maveniverse/maven/mwm/extension310/internal/MwmRepositorySystemSessionFactory.java
@@ -9,89 +9,35 @@ package eu.maveniverse.maven.mwm.extension310.internal;
 
 import static java.util.Objects.requireNonNull;
 
-import eu.maveniverse.maven.mwm.core.Workspace;
-import eu.maveniverse.maven.mwm.core.WorkspaceManager;
-import java.nio.file.Path;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import eu.maveniverse.maven.mwm.extension.shared.MwmSessionWrapper;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 import org.apache.maven.execution.MavenExecutionRequest;
 import org.apache.maven.internal.RepositorySystemSessionFactory;
 import org.apache.maven.internal.aether.DefaultRepositorySystemSessionFactory;
-import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
-import org.eclipse.aether.repository.LocalRepository;
-import org.eclipse.aether.repository.LocalRepositoryManager;
-import org.eclipse.aether.util.repository.ChainedLocalRepositoryManager;
 import org.eclipse.sisu.Priority;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Singleton
 @Named
 @Priority(100)
 final class MwmRepositorySystemSessionFactory implements RepositorySystemSessionFactory {
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
     private final DefaultRepositorySystemSessionFactory defaultFactory;
-    private final RepositorySystem repositorySystem;
-    private final WorkspaceManager workspaceManager;
+    private final MwmSessionWrapper wrapper;
 
     @Inject
     public MwmRepositorySystemSessionFactory(
-            DefaultRepositorySystemSessionFactory defaultFactory,
-            RepositorySystem repositorySystem,
-            WorkspaceManager workspaceManager) {
+            DefaultRepositorySystemSessionFactory defaultFactory, MwmSessionWrapper wrapper) {
         this.defaultFactory = requireNonNull(defaultFactory);
-        this.repositorySystem = requireNonNull(repositorySystem);
-        this.workspaceManager = requireNonNull(workspaceManager);
+        this.wrapper = requireNonNull(wrapper);
     }
 
     @Override
     public RepositorySystemSession.SessionBuilder newRepositorySessionBuilder(
             MavenExecutionRequest mavenExecutionRequest) {
-        return defaultFactory
-                .newRepositorySessionBuilder(mavenExecutionRequest)
-                .setLocalRepositoryManager(newLocalRepositoryManager(
-                        mavenExecutionRequest, defaultFactory.newRepositorySessionBuilder(mavenExecutionRequest)));
-    }
-
-    public LocalRepositoryManager newLocalRepositoryManager(
-            MavenExecutionRequest mavenExecutionRequest, RepositorySystemSession.SessionBuilder builder) {
-        try (RepositorySystemSession.CloseableSession session = builder.build()) {
-            if (session.getLocalRepositoryManager() instanceof ChainedLocalRepositoryManager) {
-                logger.info("Chained LRM detected; MWM is not interfering with it");
-                return session.getLocalRepositoryManager();
-            }
-
-            Map<String, String> configProperties = new HashMap<>();
-            session.getConfigProperties().forEach((key, value) -> {
-                if (value instanceof String) {
-                    configProperties.put(key, (String) value);
-                }
-            });
-            Workspace workspace = workspaceManager
-                    .detectWorkspace(mavenExecutionRequest.getRootDirectory(), configProperties)
-                    .orElse(null);
-            if (workspace != null) {
-                logger.info("Using MWM workspace: {}", workspace.workspaceId());
-                Path buildOutputDirectory = session.getLocalRepository()
-                        .getBasePath()
-                        .resolve(".mwn")
-                        .resolve(workspace.workspaceId());
-                return new ChainedLocalRepositoryManager(
-                        session.getLocalRepositoryManager(),
-                        Collections.singletonList(repositorySystem.newLocalRepositoryManager(
-                                session, new LocalRepository(buildOutputDirectory))),
-                        false,
-                        1,
-                        0);
-            }
-
-            return session.getLocalRepositoryManager();
-        }
+        return wrapper.wrap(
+                mavenExecutionRequest.getRootDirectory(),
+                defaultFactory.newRepositorySessionBuilder(mavenExecutionRequest));
     }
 }
